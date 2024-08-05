081bf4bd634297d1a148bcb9dbae963b6c66998c
ritikatechnical <ritikabsr@gmail.com>
2024-08-06 00:29:08 +0530

    added file

--- tests/tests/crud/test_create_booking.py
+++ tests/tests/crud/test_create_booking.py
@@ -1,12 +1,42 @@
-import requests
 import allure
 import pytest
+import logging
+from src.helpers.api_requests_wrapper import post_request
+from src.constants.api_constants import ApiConstants
+from src.helpers.payload_manager import payload_create_booking
+from src.helpers.common_verification import *
+from src.utils.utils import Utils
 
 
 class TestCreateBooking(object):
     @pytest.mark.positive
-    @allure.title("Create Booking status and verify that Booking Id should not be null")
-
+    @allure.title("Verify that Create Booking Status and Booking ID shouldn't be null")
+    @allure.description(
+        "Creating a Booking from the paylaod and verfiy that booking id should not be null and status code should be 200 for the correct payload")
     def test_create_booking_positive(self):
-        pass
+        LOGGER = logging.getLogger(__name__)
+        response = post_request(
+            url=ApiConstants().url_create_booking(),
+            auth=None,
+            headers= Utils().common_headers_json(),
+            payload=payload_create_booking(),
+            in_json=False
+        )
+        verify_http_status_code(response_data=response, expect_data=200)
+        verify_json_key_for_not_null(response.json()["bookingid"])
+        LOGGER.info(response.json()["bookingid"])
 
+    @pytest.mark.negative
+    @allure.title("Verify that Create Booking doesn't work with no payload")
+    @allure.description(
+        "Creating a Booking with no payload and verify that booking id")
+    def test_create_booking_negative(self):
+        LOGGER = logging.getLogger(__name__)
+        response = post_request(
+            url=ApiConstants().url_create_booking(),
+            auth=None,
+            headers= Utils().common_headers_json(),
+            payload={},
+            in_json=False
+        )
+        verify_http_status_code(response_data=response, expect_data=500)
